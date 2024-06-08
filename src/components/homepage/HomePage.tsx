fbcda63e482df3d23e79b654d720802d0058689f
unknown <santhoshrajrathinam@gmail.com>
2024-06-08 14:34:14 +0530

    08-06-24

--- src/components/homepage/HomePage.tsx
+++ src/components/homepage/HomePage.tsx
@@ -5,7 +5,45 @@ export const HomePage = () => {
   return (
    
         <div className="carousel-banner mt-2">
-      <CarouselItem/>
+         <CarouselItem/>
+              <div className="container mt-5">
+        <div className="row">
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <div className="border rounded border-primary d-flex align-items-center justify-content-center" style={{ width: '50px', height: '50px' }}>
+                <i className="bi bi-truck text-primary" style={{ fontSize: '30px' }}></i>
+              </div>
+              <span className="ms-2">Delivery in 30 minutes</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <div className="border rounded border-primary d-flex align-items-center justify-content-center" style={{ width: '50px', height: '50px' }}>
+                <i className="bi bi-wallet-fill text-primary" style={{ fontSize: '30px' }}></i>
+              </div>
+              <span className="ms-2">Delivery in 30 minutes</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <div className="border rounded border-primary d-flex align-items-center justify-content-center" style={{ width: '50px', height: '50px' }}>
+                <i className="bi bi-cash-coin text-primary" style={{ fontSize: '30px' }}></i>
+              </div>
+              <span className="ms-2">Delivery in 30 minutes</span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex align-items-center">
+              <div className="border rounded border-primary d-flex align-items-center justify-content-center" style={{ width: '50px', height: '50px' }}>
+                <i className="bi bi-bank text-primary" style={{ fontSize: '30px' }}></i>
+              
+              </div>
+              <span className="ms-2">Delivery in 30 minutes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
       </div>
  
   )
